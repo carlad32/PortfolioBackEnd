8764767f3971b0f336a4292d7c0e6b7aa9d1d8ee
Carla Aneley Duarte <khharrr@gmail.com>
2023-06-29 19:00:34 -0300

    agrego ruta de firebase1

--- BackEnd/src/main/java/com/portfolio/cad/Security/Controller/AuthController.java
+++ BackEnd/src/main/java/com/portfolio/cad/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200", "https://portfoliodca.web.app/"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://portfoliodca.web.app"})
 public class AuthController {
 
     @Autowired
